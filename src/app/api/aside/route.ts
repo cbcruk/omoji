7beac805f4f8305db3a40059f7e7a83dda72217e
Eunsoo Lee <cbcruk@users.noreply.github.com>
2025-04-21 22:31:20 +0900

    fix: `Effect.runPromise` pipe로 위치 변경

--- src/app/api/aside/route.ts
+++ src/app/api/aside/route.ts
@@ -4,28 +4,27 @@ import { EmojiService } from '@/services/Emoji'
 const DEFAULT_HEADERS = new Headers([['Content-Type', 'application/json']])
 
 export async function GET() {
-  return Effect.runPromise(
-    Effect.gen(function* () {
-      const emojiService = yield* EmojiService
-      const result = yield* emojiService.getTree()
+  return Effect.gen(function* () {
+    const emojiService = yield* EmojiService
+    const result = yield* emojiService.getTree()
 
-      return result
-    }).pipe(
-      Effect.provide(EmojiService.Default),
-      Effect.match({
-        onSuccess(tree) {
-          return new Response(tree, {
-            status: 200,
-            headers: DEFAULT_HEADERS,
-          })
-        },
-        onFailure(error) {
-          return new Response(JSON.stringify({ error }), {
-            status: 500,
-            headers: DEFAULT_HEADERS,
-          })
-        },
-      })
-    )
+    return result
+  }).pipe(
+    Effect.provide(EmojiService.Default),
+    Effect.match({
+      onSuccess(tree) {
+        return new Response(tree, {
+          status: 200,
+          headers: DEFAULT_HEADERS,
+        })
+      },
+      onFailure(error) {
+        return new Response(JSON.stringify({ error }), {
+          status: 500,
+          headers: DEFAULT_HEADERS,
+        })
+      },
+    }),
+    Effect.runPromise
   )
 }
